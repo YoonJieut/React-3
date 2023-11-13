b0172f087624cc03921c7413024e8d8b146580ee
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 19:35:21 +0900

    프로젝트 시작 준비 완료!

---
+++ repeat-learning-2.js
@@ -0,0 +1,21 @@
+/**
+ * todo : webserver 제작
+ * pseudo 1. express.js를 활용
+ * psuedo 2. get 요청에 대응
+ * * 문서를 조립하는 route.js 구조
+ * * 예상되는 요청 "/"
+ * 
+ * pseudo3. post에 대응한다.
+ * * action = '/mainPromptInputData"
+ * * method = "POST"
+ */
+
+/**
+ * todo : 찾아볼 기술
+ * psuedo 4. express.js
+ * * express.js의 기본적인 사용법
+ * * express static 기능
+ * * express router 기능
+ * * express middleware 기능
+ * * 찾아보면서 추가하기
+ */
